164fd27fd1c22c3fce42d163db93fbe37424ff1d
Carlos Daniel <caduccino@live.com>
2021-10-23 14:46:54 -0300

    Lab concluído

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
 const express = require("express");
-
 const PORT = 5000;
 
-// Importing all the pokemon for our data file
 const allPokemon = require("./data");
-
 const app = express();
 app.use(express.json());
 
@@ -14,7 +11,7 @@ app.get('/pokemon', (request, response) => {
 
 app.get('/pokemon/:id', (request, response) => {
     const { id } = request.params;
-    const foundPokemonById = allPokemon.find((el, index, array) => el.id.toString() === id);
+    const foundPokemonById = allPokemon.find((el) => el.id.toString() === id);
     if (!foundPokemonById) {
       return response.status(204).json({});
     }
@@ -38,18 +35,15 @@ app.get('/search', (request, response) => {
     }
 });
 
-
 app.post('/pokemon', (request, response) => {
     const { id, name, types, height, weight, sprite} = request.body;
     if (!id || !name || !types || !height || !weight || !sprite) {
       return response.status(400).json({ message: 'Favor preencher todos os dados do novo Pokemon' });
     }
-
     const pokemonExists = allPokemon.some((pokemon) => pokemon.name === name);
     if (pokemonExists) {
       return response.status(400).json({ message: 'Pokemon já existente' })
     }
-  
     const newPokemon = {
         id: new Date().getTime(),
         name: name,
@@ -58,10 +52,33 @@ app.post('/pokemon', (request, response) => {
         weight: weight,
         sprite: sprite,
     };
-  
-    allPokemon.push(newPokemon); s
-  
+    allPokemon.push(newPokemon);
     return response.status(201).json(newPokemon)
-}); 
+});
+
+app.put('/pokemon/:id', (request, response) => {
+    const { id } = request.params;
+    const { name, types, height, weight, sprite} = request.body;
+    const foundPokemon = allPokemon.find((el) => el.id.toString() === id);
+    if (!foundPokemon) {
+      return response.status(400).json({ message: `User com id ${id} não encontrado` });
+    }
+    foundPokemon.name = name;
+    foundPokemon.types = types;
+    foundPokemon.height = height;
+    foundPokemon.weight = weight;
+    foundPokemon.sprite = sprite;
+    response.json(foundPokemon);
+});
+
+app.delete('/pokemon/:id', (request, response) => {
+    const { id } = request.params;
+    const userIndex = allPokemon.findIndex((pokemon) => pokemon.id.toString() === id);
+    if (userIndex < 0) {
+      return response.status(400).json({ message: `Pokemon com id ${id} não encontrado` })
+    }
+    allPokemon.splice(userIndex, 1);
+    response.json({ message: 'Pokemon deletado com sucesso' });
+  });
 
 app.listen(PORT, () => console.log(`Server up and running at port ${PORT}`));
