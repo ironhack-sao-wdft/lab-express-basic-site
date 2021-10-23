307d5610cb5880130c749dcf2e55be52456670c0
Carlos Daniel <caduccino@live.com>
2021-10-23 12:27:36 -0300

    Feito até iterção 3

--- app.js
+++ app.js
@@ -1,12 +1,41 @@
 const express = require("express");
 
-const PORT = 4000;
+const PORT = 5000;
 
 // Importing all the pokemon for our data file
 const allPokemon = require("./data");
 
 const app = express();
+  
+app.get('/pokemon', (request, response) => {
+    return response.json(allPokemon);
+});
 
-// -- Define your route listeners here! --
+app.get('/pokemon/:id', (request, response) => {
+    const { id } = request.params;
+    const foundPokemonById = allPokemon.find((el, index, array) => el.id.toString() === id);
+    if (!foundPokemonById) {
+      return response.status(204).json({});
+    }
+    return response.json( foundPokemonById );
+});
+
+app.get('/search', (request, response) => {
+    const { name = '', types = ''} = request.query;
+    const filteredPokemonsByName = allPokemon.filter((pokemon) => {
+        return pokemon.name.toLowerCase().includes(name.toString().toLowerCase())
+    })
+    const filteredPokemonsByType = allPokemon.filter((pokemon) => {
+        return pokemon.types.find((type) => {
+            return type.toLowerCase().includes(types.toString().toLowerCase())
+        });
+    })
+    if (name !== ''){
+        return response.json(filteredPokemonsByName)
+    } else {
+        return response.json(filteredPokemonsByType)
+    }
+});
+
 
 app.listen(PORT, () => console.log(`Server up and running at port ${PORT}`));
