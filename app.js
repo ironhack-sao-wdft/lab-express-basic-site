1be84d996c0479aaa349e632c7e64c0d411e1d40
Elizeu Santos <elyzeu.tec@gmail.com>
2021-11-30 09:47:03 -0300

    solved Lab

--- app.js
+++ app.js
@@ -7,6 +7,28 @@ const allPokemon = require("./data");
 
 const app = express();
 
-// -- Define your route listeners here! --
+// -- Define your route listeners here! --npm
+app.get("/pokemon", (req, res) => {
+    return res.json(allPokemon);
+});
+
+app.get("pokemon/:id", (req, res) => {
+    const id = req.params.id;
+
+    const foundPokemon = allPokemon.find((currentPokemon) => {
+        return currentPokemon.id === Number(id);
+    });
+
+    if (foundPokemon) {
+        return res.json(foundPokemon);
+    }
+
+    return res.status(404).json({ message: "Pokemon não encontrado."});
+});
+
 
 app.listen(PORT, () => console.log(`Server up and running at port ${PORT}`));
+ 
+
+
+
